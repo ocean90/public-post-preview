3e0959bc7f075ff398054e3acf33a9043d50ea08
Matt Martz <matt@sivel.net>
2009-02-20 01:21:38 +0000

    Initial upload, tagging 1.0 for release
    git-svn-id: http://plugins.svn.wordpress.org/public-post-preview/trunk@96088 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public-post-preview.php
@@ -0,0 +1,113 @@
+<?php
+/*
+Plugin Name: Public Post Preview
+Plugin URI: http://sivel.net/wordpress/
+Description: Enables you to give a link to anonymous users for public preview of a post before it is published.
+Author: Matt Martz
+Version: 1.0
+Author URI: http://sivel.net/
+*/
+
+class Public_Post_Preview {
+
+	// Variable place holder for post ID for easy passing between functions
+	var $id;
+
+	// Plugin startup
+	function Public_Post_Preview() {
+		if (!is_admin()) {
+			add_action('init', array(&$this, 'show_preview'));
+		} else {
+			register_activation_hook(__FILE__, array(&$this, 'init'));
+			add_action('admin_menu', array(&$this, 'meta_box'));
+			add_action('save_post', array(&$this, 'save_post'));
+		}
+	}
+
+	// Initialize plugin
+	function init() {
+		if ( !get_option('public_post_preview') )
+			add_option('public_post_preview', array());
+	}
+
+	// verify a nonce
+	function verify_nonce($nonce, $action = -1) {
+		$i = wp_nonce_tick();
+		if ( substr(wp_hash($i . $action, 'nonce'), -12, 10) == $nonce )
+			return 12;
+		if ( substr(wp_hash(($i - 1) . $action, 'nonce'), -12, 10) == $nonce )
+			return 24;
+		return false;
+	}
+
+	// create a nonce
+	function create_nonce($action = -1) {
+		$i = wp_nonce_tick();
+		return substr(wp_hash($i . $action, 'nonce'), -12, 10);
+	}
+
+	// Content for meta box
+	function preview_link($post) {
+		$preview_posts = get_option('public_post_preview');
+		if ( in_array($post->post_status, array('draft', 'pending')) ) {
+?>
+			<p>
+				<label for="public_preview_status" class="selectit">
+					<input type="checkbox" name="public_preview_status" id="public_preview_status"<?php if ( !isset($preview_posts[$post->ID]) ) echo ' checked="checked"'; ?>/>
+					Public Preview
+				</label>
+			</p>
+<?php
+			if ( !isset($preview_posts[$post->ID]) ) {
+				$this->id = (int) $post->ID;
+				$nonce = $this->create_nonce('public_post_preview_' . $this->id);
+
+				$url = htmlentities(add_query_arg(array('preview' => 'true', 'preview_id' => $this->id, 'public' => true, 'nonce' => $nonce), get_permalink($this->id)));
+
+				echo "<p><a href='$url'>$url</a><br /><br />\r\n";
+			}
+		} else {
+			echo '<p>This post is already public.  Public post preview not available.</p>';
+		}
+	}
+
+	// Register meta box
+	function meta_box() {
+		add_meta_box('publicpostpreview', 'Public Post Preview', array(&$this, 'preview_link'), 'post', 'normal', 'high');
+	}
+
+	// Update options on post save
+	function save_post($post) {
+		$preview_posts = get_option('public_post_preview');
+		$post_id = $_POST['post_ID'];
+		if ( $post != $post_id )
+			return;
+		if ( (isset($_POST['public_preview_status']) && $_POST['public_preview_status'] == 'on') && in_array($_POST['post_status'], array('draft', 'pending')) && isset($preview_posts[$post_id]) ) {
+				unset($preview_posts[$post_id]);
+		} elseif ( !isset($_POST['public_preview_status']) && $_POST['original_post_status'] != 'publish' && in_array($_POST['post_status'], array('draft', 'pending')) && !isset($preview_posts[$post_id]) ) {
+				$preview_posts[$post_id] = false;
+		}
+		update_option('public_post_preview', $preview_posts);
+	}
+
+	// Show the post preview
+	function show_preview() {
+		if ( !is_admin() && isset($_GET['p']) && isset($_GET['preview_id']) && isset($_GET['preview']) && isset($_GET['public']) && isset($_GET['nonce']) ) {
+			$this->id = (int) $_GET['preview_id'];
+			$preview_posts = get_option('public_post_preview');	
+
+			if ( false == $this->verify_nonce($_GET['nonce'], 'public_post_preview_' . $this->id) || isset($preview_posts[$this->id]) )
+				wp_die('You do not have permission to publicly preview this post.');
+	
+			add_filter('the_posts', array(&$this, 'insert_preview'));
+		}
+	}
+
+	// Insert the requested preview post into posts array
+	function insert_preview($posts) {
+		$posts[] = get_post($this->id);
+		return $posts;
+	}
+}
+$public_post_preview = new Public_Post_Preview();
+?>
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Public Post Preview ===
+Contributors: sivel
+Donate Link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=C3UA7TDWM4NLQ&lc=US&item_name=Donations%20for%20Sivel%2enet%20WordPress%20Plugins&cn=Add%20special%20instructions%20to%20the%20seller&no_shipping=1&rm=1&return=http%3a%2f%2fsivel%2enet%2fthanks&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Tags: public-post-preview, public, post, preview, posts
+Requires at least: 2.7
+Tested up to: 2.7
+Stable tag: 1.0
+
+Enables you to give a link to anonymous users for public preview of a post before it is published.
+
+== Description ==
+
+Enables you to give a link to anonymous users for public preview of a post before it is published.
+
+Have you ever been writing a post with the help of someone who does not have access to your blog and needed to give them the ability to preview it before publishing? This plugin takes care of that by generating a URL with an expiring nonce that can be given out for public preview.
+
+Props to [Jonathan Dingman](http://www.ginside.com/) for the idea behind this plugin, testing and feedback.
+
+== Installation ==
+
+1. Upload the `public-post-preview` folder to the `/wp-content/plugins/` directory or install directly through the plugin installer.
+1. Activate the plugin through the 'Plugins' menu in WordPress or by using the link provided by the plugin installer
+
+NOTE: See "Other Notes" for Upgrade and Usage Instructions as well as other pertinent topics.
+
+== Screenshots ==
+
+1. Edit Posts Page
+
+== Upgrade ==
+
+1. Use the plugin updater in WordPress or...
+1. Delete the previous `public-post-preview` folder from the `/wp-content/plugins/` directory
+1. Upload the new `public-post-preview` folder to the `/wp-content/plugins/` directory
+
+== Usage ==
+
+1. By default all posts in draft or pending review status will have public preview links that can be found diretly below the edit post box.
+1. To disable public post preview for a specific post uncheck the public preview post box and save the post.
+
+== Change Log ==
+
+= 1.0 (2009-02-20): =
+* Initial Public Release
