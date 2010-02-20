8fb576ee2c436b46a8a7cf68ff8e07ebf221105b
Matt Martz <matt@sivel.net>
2010-02-20 17:55:54 +0000

    Some more improvements
    git-svn-id: http://plugins.svn.wordpress.org/public-post-preview/trunk@208406 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public-post-preview.php
+++ public-post-preview.php
@@ -3,7 +3,7 @@
 Plugin Name: Public Post Preview
 Plugin URI: http://sivel.net/wordpress/
 Description: Enables you to give a link to anonymous users for public preview of a post before it is published.
-Author: Matt Martz</a> and <a href='http://www.ginside.com/'>Jonathan Dingman</a>
+Author: Matt Martz</a> and <a href='http://wpvibe.com/'>Jonathan Dingman</a>
 Version: 1.4
 Author URI: http://sivel.net/
 */
@@ -23,7 +23,25 @@ class Public_Post_Preview {
 			add_action('admin_menu', array(&$this, 'add_options_page')) ;
 			add_action('admin_menu', array(&$this, 'meta_box'));
 			add_action('save_post', array(&$this, 'save_post'));
+			add_action('admin_footer-post.php', array(&$this, 'footer_scripts'));
+			add_action('admin_footer-post-new.php', array(&$this, 'footer_scripts'));
+			add_action('wp_ajax_pppgetlink', array(&$this, 'ajax_get_preview_link'));
 		}
+	}
+
+	function footer_scripts() {
+?>
+<script type="text/javascript">
+	jQuery("#getpreviewlink").click(function() {
+		jQuery.get('<?php echo admin_url('admin-ajax.php'); ?>', 'action=pppgetlink&p=<?php echo $this->id; ?>&_wpnonce=<?php echo wp_create_nonce(); ?>', function(link) {
+			if ( link != '' ) {
+				jQuery('#previewlink').attr('href', link);
+				jQuery('#previewlink').text(link);
+			}
+		});
+	});
+</script>
+<?php
 	}
 
 	// Initialize plugin
@@ -31,7 +49,7 @@ class Public_Post_Preview {
 		if ( ! get_option('public_post_preview') )
 			add_option('public_post_preview', array());
 		if ( ! get_option('ppp_opts') ) {
-			$ppp_opts['time'] = 24;
+			$ppp_opts['nonce'] = 'true';
 		}
 	}
 
@@ -51,10 +69,30 @@ class Public_Post_Preview {
 		return substr(wp_hash($i . $action, 'nonce'), -12, 10);
 	}
 
+	function ajax_get_preview_link() {
+		if ( current_user_can('edit_posts') && wp_verify_nonce($_GET['_wpnonce']) ) {
+			if ( ! isset( $_GET['p'] ) )
+				die();
+			$post = get_post( $_GET['p'] );
+			$this->id = (int) $post->ID;
+			$nonce = $this->create_nonce('public_post_preview_' . $this->id);
+
+			$p = $post->post_type == 'page' ? 'page_id' : 'p';
+			echo $this->get_preview_link($p, $nonce);
+		}
+		die();
+	}
+
+	function get_preview_link($p, $nonce) {
+		return add_query_arg(array($p => $this->id, 'preview' => 'true', 'preview_id' => $this->id, 'public' => true, 'nonce' => $nonce), get_option('home') . '/');
+	}
+
 	// Content for meta box
-	function preview_link($post) {
+	function preview_link_box($post) {
 		$preview_posts = get_option('public_post_preview');
+		//echo '<pre>'; var_dump($post); echo '</pre>';
 		if ( ! in_array($post->post_status, array('publish')) ) {
+			if ( $post->ID != 0 && $post->post_status != 'auto-draft' ) :
 ?>
 			<p>
 				<label for="public_preview_status" class="selectit">
@@ -63,16 +101,18 @@ class Public_Post_Preview {
 				</label>
 			</p>
 <?php
-			if ( ! isset($preview_posts[$post->ID]) && $post->ID != 0 ) {
+			endif;
+			if ( ! isset($preview_posts[$post->ID]) && $post->ID != 0 && $post->post_status != 'auto-draft' ) {
 				$this->id = (int) $post->ID;
 				$nonce = $this->create_nonce('public_post_preview_' . $this->id);
 
 				$p = $post->post_type == 'page' ? 'page_id' : 'p';
 
-				$url = htmlentities(add_query_arg(array($p => $this->id, 'preview' => 'true', 'preview_id' => $this->id, 'public' => true, 'nonce' => $nonce), get_option('home') . '/'));
+				$url = $this->get_prevew_link($p, $nonce);
 
-				echo "<p><a href='$url'>$url</a></p>\r\n";
-			} else if ( $post->ID == 0	) {
+				echo "<p><a id='previewlink' href='$url'>$url</a></p>\n";
+				echo "<a id='getpreviewlink' class='button' href='#'>Update Preview Link</a>";
+			} else if ( $post->ID == 0 || $post->post_status == 'auto-draft' ) {
 				echo "<p>Please save this post to get the preview url.</p>";
 			}
 		} else {
@@ -82,8 +122,8 @@ class Public_Post_Preview {
 
 	// Register meta box
 	function meta_box() {
-		add_meta_box('publicpostpreview', 'Public Post Preview', array(&$this, 'preview_link'), 'post', 'normal', 'high');
-		add_meta_box('publicpostpreview', 'Public Post Preview', array(&$this, 'preview_link'), 'page', 'normal', 'high');
+		add_meta_box('publicpostpreview', 'Public Post Preview', array(&$this, 'preview_link_box'), 'post', 'normal', 'high');
+		add_meta_box('publicpostpreview', 'Public Post Preview', array(&$this, 'preview_link_box'), 'page', 'normal', 'high');
 	}
 
 	// Update options on post save
@@ -143,9 +183,13 @@ class Public_Post_Preview {
 			<h2><?php _e( 'Public Post Preview' , 'public-post-preview' ); ?></h2>
 			<form action="options.php" method="post">
 				<?php settings_fields('ppp_opts'); ?>
-				<h3><?php _e( 'General Configuration' , 'geekga' ); ?></h3>
-				<p><?php _e( 'Please enter the length of time to keep link active:' , 'public-post-preview' ); ?></p>
-				<p><input type="text" name="ppp_opts[time]" value="<?php echo $options['time']; ?>" /></p>
+				<h3><?php _e( 'General Configuration' , 'public-post-preview' ); ?></h3>
+				<p><?php _e( 'Disable funky time length?' , 'public-post-preview' ); ?></p>
+				<select name="ppp_opts[nonce]">
+					<option value="true"<?php selected ( 'true' , $options['nonce'] ); ?>><?php _e( 'true' , 'public-post-preview' ); ?></option>
+					<option value="false"<?php selected ( 'false' , $options['nonce'] ); ?>><?php _e( 'false' , 'public-post-preview' ); ?></option>
+				</select>
+				<p>The current time length is a little funky.  It is based on the nonce functions that WordPress uses, but isn't really good for exact time calculation.  The current implementation allows the link to remain active for up to 24 hours.  This doesn't mean it will last 24 hours.  It could be 3 minutes.  So feel free to disable this.  Disabling reduces security a bit.</p>
 				<p class="submit">
 					<input type="submit" class="button-primary" value="<?php _e( 'Save Changes' , 'public-post-preview' ) ?>" />
 				</p>
