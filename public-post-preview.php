6ea90976b48a47eff737d7cb39db653734ed2f15
bjoerne <bjoern_weinbrenner@yahoo.de>
2013-10-27 12:18:13 +0100

    Add options page to configure expiration time in hours

--- public-post-preview.php
+++ public-post-preview.php
@@ -38,6 +38,8 @@ if ( ! class_exists( 'WP' ) ) {
 	die();
 }
 
+include_once dirname( __FILE__ ).'/options.php';
+
 /**
  * The class which controls the plugin.
  *
@@ -72,6 +74,10 @@ class DS_Public_Post_Preview {
 
 			add_filter( 'query_vars', array( __CLASS__, 'add_query_var' ) );
 		} else {
+			add_action('admin_init' , array( __CLASS__, 'register_setting' ) );
+
+			add_action('admin_menu' , array( __CLASS__, 'add_options_page' ) );
+
 			add_action( 'post_submitbox_misc_actions', array( __CLASS__, 'post_submitbox_misc_actions' ) );
 
 			add_action( 'save_post', array( __CLASS__, 'register_public_preview' ), 20, 2 );
@@ -80,6 +86,22 @@ class DS_Public_Post_Preview {
 
 			add_action( 'admin_enqueue_scripts' , array( __CLASS__, 'enqueue_script' ) );
 		}
+	}
+
+	/**
+	 * Registers the expiration hours setting.
+	 *
+	 */
+	public static function register_setting() {
+		register_setting('public_post_preview_group', 'public_post_preview_expiration_hours');
+	}
+
+	/**
+	 * Adds the option page to the menu.
+	 *
+	 */
+	public static function add_options_page() {
+		add_options_page(__('Public Post Preview'), __('Public Post Preview'), 'manage_options', basename(__FILE__), 'public_post_preview_show_settings_page');
 	}
 
 	/**
@@ -389,7 +411,11 @@ class DS_Public_Post_Preview {
 	 * @return int The time-dependent variable
 	 */
 	private static function nonce_tick() {
-		$nonce_life = apply_filters( 'ppp_nonce_life', 60 * 60 * 48 ); // 48 hours
+		$expiration_hours = (int) get_option('public_post_preview_expiration_hours');
+		if ($expiration_hours <= 0) {
+			$expiration_hours = 48;
+		}
+		$nonce_life = apply_filters( 'ppp_nonce_life', 60 * 60 * $expiration_hours );
 
 		return ceil( time() / ( $nonce_life / 2 ) );
 	}
