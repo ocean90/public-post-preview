72de817117f331066c40a199a0736748ace22f10
Dominik Schilling <dominikschilling+git@gmail.com>
2018-09-13 17:41:36 +0900

    Add makepot commands

---
+++ languages/public-post-preview.php
@@ -0,0 +1,16 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: js/src/components/preview-toggle/index.js:113
+	__( 'Enable Public Preview', 'public-post-preview' ),
+
+	// Reference: js/src/components/preview-toggle/index.js:121
+	__( 'Preview URL', 'public-post-preview' ),
+
+	// Reference: js/src/components/preview-toggle/index.js:132
+	__( 'Copy link to clipboard', 'public-post-preview' ),
+
+	// Reference: js/src/components/preview-toggle/index.js:138
+	__( 'Copy and share this preview link.', 'public-post-preview' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
