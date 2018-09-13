f18b9d60573b99bb5d0fba03b269ab7d0c371310
Dominik Schilling <dominikschilling+git@gmail.com>
2018-09-13 17:12:51 +0900

    Use a button to copy the link to clipboard

--- js/src/components/preview-toggle/index.js
+++ js/src/components/preview-toggle/index.js
@@ -1,5 +1,8 @@
 import { __ } from '@wordpress/i18n';
-import { CheckboxControl } from '@wordpress/components';
+import {
+	CheckboxControl,
+	IconButton,
+} from '@wordpress/components';
 import {
 	Component,
 	Fragment,
@@ -20,13 +23,21 @@ const pluginPostStatusInfoPreviewUrl = css`
 `
 
 const pluginPostStatusInfoPreviewUrlInput = css`
-	cursor: pointer;
+	width: 100%;
+	margin-right: 12px;
 `
 
 const pluginPostStatusInfoPreviewDescription = css`
 	font-style: italic;
 	color: #666;
 	margin: .2em 0 0 !important;
+`
+
+const pluginPostStatusInfoPreviewUrlInputWrapper = css`
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	margin: 0;
 `
 
 class PreviewToggle extends Component {
@@ -37,20 +48,16 @@ class PreviewToggle extends Component {
 		this.state = {
 			previewEnabled: DSPublicPostPreviewData.previewEnabled,
 			previewUrl: DSPublicPostPreviewData.previewUrl,
-			urlCopied: false,
 		}
 
 		this.previewUrlInput = React.createRef();
 
 		this.onChange = this.onChange.bind( this );
-		this.onFocus = this.onFocus.bind( this );
-		this.onClick = this.onClick.bind( this );
+		this.onPreviewUrlInputFocus = this.onPreviewUrlInputFocus.bind( this );
+		this.onCopyLinkButtonClick = this.onCopyLinkButtonClick.bind( this );
 	}
 
 	componentWillUnmount() {
-		if ( this.timerId ) {
-			clearTimeout( this.timerId );
-		}
 	}
 
 	onChange( checked ) {
@@ -62,23 +69,14 @@ class PreviewToggle extends Component {
 		} )
 	}
 
-	onFocus() {
+	onPreviewUrlInputFocus() {
 		this.previewUrlInput.current.focus();
 		this.previewUrlInput.current.select();
 	}
 
-	onClick() {
-		if ( this.timerId ) {
-			clearTimeout( this.timerId );
-		}
-
+	onCopyLinkButtonClick() {
 		this.previewUrlInput.current.select();
-		const copied = document.execCommand( 'copy' );
-		this.setState( { urlCopied: copied } );
-
-		if ( copied ) {
-			this.timerId = setTimeout( () => this.setState( { urlCopied: false } ), 2000 );
-		}
+		document.execCommand( 'copy' );
 	}
 
 	/**
@@ -106,7 +104,6 @@ class PreviewToggle extends Component {
 		const {
 			previewEnabled,
 			previewUrl,
-			urlCopied,
 		} = this.state;
 
 		return (
@@ -120,20 +117,25 @@ class PreviewToggle extends Component {
 				</PluginPostStatusInfo>
 				{ previewEnabled &&
 					<PluginPostStatusInfo className={ pluginPostStatusInfoPreviewUrl }>
-						<label htmlFor="public-post-preview-url" className="screen-reader-text">{ __( 'Preview URL', 'public-post-preview' ) }</label>
-						<input
-							ref={ this.previewUrlInput }
-							type="text"
-							id="public-post-preview-url"
-							className={ pluginPostStatusInfoPreviewUrlInput }
-							value={ previewUrl }
-							readOnly
-							onFocus={ this.onFocus }
-							onClick={ this.onClick }
-						/>
+						<p className={ pluginPostStatusInfoPreviewUrlInputWrapper }>
+							<label htmlFor="public-post-preview-url" className="screen-reader-text">{ __( 'Preview URL', 'public-post-preview' ) }</label>
+							<input
+								ref={ this.previewUrlInput }
+								type="text"
+								id="public-post-preview-url"
+								className={ pluginPostStatusInfoPreviewUrlInput }
+								value={ previewUrl }
+								readOnly
+								onFocus={ this.onPreviewUrlInputFocus }
+							/>
+							<IconButton
+								label={ __( 'Copy link to clipboard', 'public-post-preview' ) }
+								icon={ <svg width="20" height="20" viewBox="0 0 14 16" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" focusable="false" ><path fillRule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg> }
+								onClick={ this.onCopyLinkButtonClick }
+							/>
+						</p>
 						<p className={ pluginPostStatusInfoPreviewDescription }>
-							{ __( '(Click to copy and share the link.)', 'public-post-preview' ) }
-							{ urlCopied && <strong>{ ' ' + __( 'Copied!', 'public-post-preview' ) }</strong> }
+							{ __( 'Copy and share this preview link.', 'public-post-preview' ) }
 						</p>
 					</PluginPostStatusInfo>
 				}
