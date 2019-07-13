6080b3c42ee6f9805f12746ee70e5522fe3d753b
Dominik Schilling <dominikschilling+git@gmail.com>
2019-07-13 11:27:54 +0200

    Update dependencies to latest

--- js/src/components/preview-toggle/index.js
+++ js/src/components/preview-toggle/index.js
@@ -14,7 +14,6 @@ import {
 import {
 	Component,
 	createRef,
-	Fragment,
 } from '@wordpress/element';
 import { withSelect } from '@wordpress/data';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
@@ -48,6 +47,8 @@ const pluginPostStatusInfoPreviewUrlInputWrapper = css`
 	align-items: center;
 	margin: 0;
 `
+
+const ClipboardIcon = <SVG width="20" height="20" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg"><Path fillRule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"/></SVG>
 
 class PreviewToggle extends Component {
 
@@ -103,7 +104,7 @@ class PreviewToggle extends Component {
 		const ariaCopyLabel = hasCopied ? __( 'Preview URL copied', 'public-post-preview' ) : __( 'Copy the preview URL', 'public-post-preview' );
 
 		return (
-			<Fragment>
+			<>
 				<PluginPostStatusInfo>
 					<CheckboxControl
 						label={ __( 'Enable public preview', 'public-post-preview' ) }
@@ -130,7 +131,7 @@ class PreviewToggle extends Component {
 								onCopy={ () => this.setState( { hasCopied: true } ) }
 								onFinishCopy={ () => this.setState( { hasCopied: false } ) }
 								aria-disabled={ hasCopied }
-								icon={ <SVG width="20" height="20" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg" focusable="false" ><Path fillRule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"/></SVG> }
+								icon={ ClipboardIcon }
 							/>
 						</p>
 						<p className={ pluginPostStatusInfoPreviewDescription }>
@@ -138,7 +139,7 @@ class PreviewToggle extends Component {
 						</p>
 					</PluginPostStatusInfo>
 				}
-			</Fragment>
+			</>
 		);
 	}
 }
