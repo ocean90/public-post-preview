7c7a2462079419753834e65ba916a8eeabd7b63b
Dominik Schilling <dominikschilling+git@gmail.com>
2020-10-03 13:03:04 +0200

    Set allowedTextDomain for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,12 @@
 const defaultConfig = require( './node_modules/@wordpress/scripts/config/.eslintrc.js' );
-module.exports = defaultConfig;
+
+module.exports = {
+	...defaultConfig,
+	rules: {
+		...defaultConfig.rules,
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{ allowedTextDomain: [ 'public-post-preview' ] },
+		],
+	},
+};
