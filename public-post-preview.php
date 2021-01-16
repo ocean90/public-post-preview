627b0eb69942a296ac0a421e0708434f8f77c11e
Dominik Schilling <dominikschilling+git@gmail.com>
2021-01-16 15:41:04 +0100

    Update 'Tested up to' for 5.6

--- public-post-preview.php
+++ public-post-preview.php
@@ -8,12 +8,13 @@
  * Plugin URI: https://dominikschilling.de/wp-plugins/public-post-preview/en/
  * Text Domain: public-post-preview
  * Requires at least: 5.0
+ * Tested up to: 5.6
  * Requires PHP: 5.6
  * License: GPLv2 or later
  *
  * Previously (2009-2011) maintained by Jonathan Dingman and Matt Martz.
  *
- *  Copyright (C) 2012-2020 Dominik Schilling
+ *  Copyright (C) 2012-2021 Dominik Schilling
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
