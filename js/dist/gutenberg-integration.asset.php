61d1c2cbc127299755b271b450c880db120b311c
Dominik Schilling <dominikschilling+git@gmail.com>
2021-01-16 14:50:36 +0100

    Add linting workflow (#71)
    * Add linting workflow
    
    * Use Node LTS
    
    * Bump @wordpress/env from 2.0.1 to 3.0.0
    
    * [auto] Update assets
    
    * Bump @wordpress/scripts from 12.5.0 to 12.6.1
    
    * [auto] Update assets
    
    * Use npm ci
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- js/dist/gutenberg-integration.asset.php
+++ js/dist/gutenberg-integration.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => 'b0010caf9e1f1e1cf68fdd9337a292ba');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => 'f1da333851e877700cc3939e620f47b7');
