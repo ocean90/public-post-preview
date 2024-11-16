572dd51f2b5339cc2b6f69af21752be2f0307fd5
Dominik Schilling <dominikschilling+git@gmail.com>
2024-11-16 14:22:15 +0100

    Update dependencies

--- js/dist/gutenberg-integration.asset.php
+++ js/dist/gutenberg-integration.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'cc95da5a20e04227eb45');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '9bd0dc0c2bb048404d6c');
